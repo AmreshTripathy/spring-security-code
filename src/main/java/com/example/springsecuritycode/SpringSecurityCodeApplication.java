77182c92cdef3f5d1edbec1493b15edbf05f2a03
AmreshTripathy <amreshtripathy184@gmail.com>
2023-07-27 22:59:45 +0530

    Created Repository

---
+++ src/main/java/com/example/springsecuritycode/SpringSecurityCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCodeApplication.class, args);
+	}
+
+}
