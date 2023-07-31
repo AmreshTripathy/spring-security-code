9cdae3f2b5ce869fc2d4f0cbd756b40ba0afb5c7
AmreshTripathy <amreshtripathy184@gmail.com>
2023-07-31 22:16:42 +0530

    Added SecurityFilterChain and Encoding

--- src/main/java/com/example/springsecuritycode/SecurityConfig.java
+++ src/main/java/com/example/springsecuritycode/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -22,16 +24,19 @@ public class SecurityConfig {
 
     @Bean
     // User information
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
         UserDetails admin = User.withUsername("amresh")
-                .password("pwd1")
+                .password(encoder.encode("pwd1"))
                 .roles("ADMIN")
                 .build();
 
         UserDetails user = User.withUsername("akash")
-                .password("pwd2")
+                .password(encoder.encode("pwd2"))
                 .roles("USER", "ADMIN", "HR")
                 .build();
+
+        System.out.println("Admin : " + admin);
+        System.out.println("User : " + user);
 
         return new InMemoryUserDetailsManager(admin, user);
     }
@@ -40,12 +45,19 @@ public class SecurityConfig {
     // And which all api should not be given security
     // By default all of them are given security
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
-//        return httpSecurity.csrf().disable().authorizeRequests().requestMatchers("/welcome")
-//                .permitAll().and().authorizeRequests().requestMatchers("/");
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/home/welcome").permitAll()
+                .and()
+                .authorizeHttpRequests().requestMatchers("/home/**")
+                .authenticated().and().formLogin().and().build();
+    }
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
--- src/main/java/com/example/springsecuritycode/UserController.java
+++ src/main/java/com/example/springsecuritycode/UserController.java
@@ -1,5 +1,6 @@
 package com.example.springsecuritycode;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +19,13 @@ public class UserController {
     }
 
     @GetMapping("/admin") // This api should be under the admin role only
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public String getAdmin() {
         return "Hi Admin, you have been authenticated successfully";
     }
 
-    @GetMapping("/User")
+    @GetMapping("/user")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public String getUser() {
         return "Hi User, You have logged in successfully";
     }
